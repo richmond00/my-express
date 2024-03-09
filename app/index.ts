c3b511766619392ef703c86bb46e7c496071247a
Dongwook Kim <dongwook227@gmail.com>
2024-03-09 13:19:31 -0500

    add redis api

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,39 @@
 import express from "express"
+import * as redis from "redis"
 
-const app = express()
-
-app.use(express.json())
-app.get("/", (request, response) => {
-  response.status(200).send("hello from express")
-})
 const PORT = 4000;
-app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`)
-})
+const LIST_KEY = "messages";
 
+const createApp = async () => {
+  const app = express()
+
+  const client = redis.createClient({ url: "redis://localhost:6379" })
+  await client.connect();
+
+  app.use(express.json())
+  app.get("/", (request, response) => {
+    response.status(200).send("hello from express")
+  })
+
+  app.post("/messages", async (request, response) => {
+    const { message } = request.body;
+    await client.lPush(LIST_KEY, message);
+    response.status(200).send("Message added to list")
+  });
+
+  app.get("/messages", async (resquest, response) => {
+    const messages = await client.lRange(LIST_KEY, 0, -1);
+    response.status(200).send(messages)
+
+  });
+
+
+
+  return app; 
+}
+
+createApp().then((app) => {
+  app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`)
+  });
+});
