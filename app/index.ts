f7e92a9bbf21883bae69b0b6d140981f5536070d
Dongwook Kim <dongwook227@gmail.com>
2024-03-20 08:26:36 -0400

    restructure for test

--- app/index.ts
+++ app/index.ts
@@ -1,39 +1,16 @@
-import express from "express"
-import * as redis from "redis"
+import * as redis from "redis";
+import { createApp } from './app';
 
 const PORT = 4000;
-const LIST_KEY = "messages";
 
-const createApp = async () => {
-  const app = express()
-
-  const client = redis.createClient({ url: "redis://localhost:6379" })
+const startServer = async () => {
+  const client = redis.createClient({ url: "redis://localhost:6379"});
   await client.connect();
 
-  app.use(express.json())
-  app.get("/", (request, response) => {
-    response.status(200).send("hello from express")
-  })
-
-  app.post("/messages", async (request, response) => {
-    const { message } = request.body;
-    await client.lPush(LIST_KEY, message);
-    response.status(200).send("Message added to list")
+  const app = createApp(client);
+  app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`);
   });
-
-  app.get("/messages", async (resquest, response) => {
-    const messages = await client.lRange(LIST_KEY, 0, -1);
-    response.status(200).send(messages)
-
-  });
-
-
-
-  return app; 
 }
 
-createApp().then((app) => {
-  app.listen(PORT, () => {
-    console.log(`App listening at port ${PORT}`)
-  });
-});
+startServer();
